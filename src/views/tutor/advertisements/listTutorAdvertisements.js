c092251bbc7ab58c65155f101d7158d817c6196a
MlamliLolwane <mrlolwane96@gmail.com>
2022-04-05 14:23:11 +0200

    05 April 2022

--- src/views/tutor/advertisements/listTutorAdvertisements.js
+++ src/views/tutor/advertisements/listTutorAdvertisements.js
@@ -22,7 +22,7 @@ function ListTutorAdvertisements() {
             const response = await TutorAdvertisementService.list(1);
 
             console.log(response.data.length);
-            
+
             setTutorAdvertisement(response.data);
 
             setActive(false);
@@ -49,7 +49,7 @@ function ListTutorAdvertisements() {
 
                 <hr className="my-0 py-0" />
 
-                <div style={{ minHeight: "100vh", backgroundColor: "black" }} className="justify-content-md-center px-0 align-items-center">
+                <div style={{ minHeight: "100vh", backgroundColor: "black" }} className="col justify-content-md-center px-0 d-flex align-items-center">
                     <br />
                     {/* <div className="row mx-auto">
                         <div className="col-4 offset-md-2">
@@ -66,21 +66,21 @@ function ListTutorAdvertisements() {
                     {tutorAdvertisement ?
                         tutorAdvertisement.map(advertisement => (
                             <div className="row grid-container" key={advertisement.id}>
-                                <div className="col-7 mx-auto mb-4">
+                                <div className="col-5 mx-auto mb-4">
                                     <div className="card mb-3 h-100">
                                         <div className="row">
-                                            <div className="col-md-4">
-                                                <img src={PlaceholderImage}
-                                                    className="img-fluid" alt="Advertisement Image" />
-                                            </div>
-                                            <div className="col-md-8">
+                                            <div className="col-md-12">
                                                 <div className="card-body">
                                                     <h5 className="card-title fw-bold text-green pb-1">R {advertisement.price},00</h5>
                                                     <p className="card-text fw-bolder mb-0 pb-2"
                                                         style={{ textTransform: "capitalize" }}>
-                                                        {advertisement.title}
+                                                        <u>{advertisement.title}</u>
                                                     </p>
-                                                    <p className="card-text mb-0 pb-2">Duration: {advertisement.duration} Minutes</p>
+                                                    <div className="card-text mb-0 pb-2 max-lines fw-lighter mb-2"
+                                                    >
+                                                        {advertisement.content}
+                                                    </div>
+                                                    <p className="card-text mb-0 pb-2 fw-bold">Duration: {advertisement.duration} Minutes</p>
                                                     <p className="card-text"><small className="text-muted"><Link to="/">
                                                         Biggie Smalls</Link></small></p>
                                                 </div>
@@ -89,60 +89,9 @@ function ListTutorAdvertisements() {
                                     </div>
                                 </div>
                             </div>
-                        )): <h1>
-                                No data to display
+                        )) : <h1>
+                            No data to display
                             </h1>}
-
-                    {/* <div className="grid-container">
-                        <div className="item1">
-                            <div className="col-7 mx-auto">
-                                <div className="card mb-3 h-100">
-                                    <div className="row">
-                                        <div className="col-md-4">
-                                            <img src={PlaceholderImage}
-                                                className="img-fluid" alt="Advertisement Image" />
-                                        </div>
-                                        <div className="col-md-8">
-                                            <div className="card-body">
-                                                <h5 className="card-title fw-bold text-green pb-1">R 150,00</h5>
-                                                <p className="card-text fw-bolder mb-0 pb-2"
-                                                    style={{ textTransform: "capitalize" }}>
-                                                    Software development tutorial from an experienced developer
-                                            </p>
-                                                <p className="card-text mb-0 pb-2">Duration: 1 hour 30 minutes</p>
-                                                <p className="card-text"><small className="text-muted"><Link to="/">
-                                                    Biggie Smalls</Link></small></p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="item2"><div className="card mb-3 h-100">
-                            <div className="col-7 mx-auto">
-                                <div className="row">
-                                    <div className="col-md-4">
-                                        <img src={PlaceholderImage2}
-                                            className="img-fluid" alt="Advertisement Image" />
-                                    </div>
-                                    <div className="col-md-8">
-                                        <div className="card-body">
-                                            <h5 className="card-title fw-bold text-green pb-1">R 150,00</h5>
-                                            <p className="card-text fw-bolder mb-0 pb-2"
-                                                style={{ textTransform: "capitalize" }}>
-                                                Software development tutorial from an experienced developer
-                                            </p>
-                                            <p className="card-text mb-0 pb-2">Duration: 1 hour 30 minutes</p>
-                                            <p className="card-text"><small className="text-muted"><Link to="/">
-                                                Biggie Smalls</Link></small></p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div></div> <br />
-                        <div className="item3">3</div>
-                    </div> */}
-
                 </div>
             </div>
         </LoadingOverlay>
---
+++ src/views/tutor/schedule/updateTutorSchedule.js
@@ -0,0 +1,179 @@
+import Navbar from '../../../components/tutor/tutorNavbar'
+import LoadingOverlay from 'react-loading-overlay-ts'
+import HashLoader from 'react-spinners/HashLoader'
+import { Link, useNavigate } from 'react-router-dom'
+import React, {useEffect} from 'react'
+import { Formik, Field, Form, ErrorMessage } from 'formik'
+import * as Yup from 'yup'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import TutorScheduleService from '../../../services/tutor/TutorScheduleService'
+
+function UpdateTutorSchedule() {
+    const [isActive, setActive] = React.useState(false);
+    const [formValues, setFormValues] = React.useState([{ start_time: "", end_time: "" }])
+    const [col, setCol] = React.useState("col-0")
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        //For each start and end time, dynamically create a form and insert the schedule on the
+    })
+
+    //Create dynamic form 
+    let addFormFields = () => {
+        setCol("col-2");
+        setFormValues([...formValues, { start_time: "", end_time: "" }])
+    }
+
+    let removeFormFields = (i) => {
+        let newFormValues = [...formValues];
+        newFormValues.splice(i, 1);
+        setFormValues(newFormValues)
+
+        if (formValues.length == 2) {
+            setCol("col-0");
+        }
+    }
+
+    //Handle input change
+    let handleChange = (i, e) => {
+        let newFormValues = [...formValues];
+        newFormValues[i][e.target.name] = e.target.value;
+        setFormValues(newFormValues);
+    }
+
+    //Submit the form
+    let handleSubmit = async (e) => {
+        try {
+            setActive(true);
+
+            e.preventDefault();
+
+            await TutorScheduleService.store({
+                tutor_id: 1,
+                day_id: 2,
+                schedule: JSON.stringify(formValues)
+            })
+
+            setActive(false);
+
+            navigate("/tutor/schedule/view")
+        } catch (error) {
+
+        }
+    }
+
+    return (
+        <Formik
+            initialValues={{
+                start_time: '',
+                end_time: '',
+            }}
+
+
+        >
+            <LoadingOverlay
+                active={isActive}
+                fadeSpeed={100}
+                styles={{
+                    overlay: (base) => ({
+                        ...base,
+                        background: ' rgba(0, 0, 0, 0.9)'
+                    })
+                }}
+                spinner={<HashLoader color="white" size="70px" speedMultiplier="2" />}
+            >
+                <div>
+                    <Navbar />
+
+                    <hr className="my-0 py-0" />
+                    <div className="row">
+
+                        <div style={{ minHeight: "100vh", backgroundColor: "black" }} className="col justify-content-md-center px-0 d-flex align-items-center">
+                            <div className="col-lg-5 col-11 col-md-8 mx-auto px-5 py-4 my-md-5 my-lg-0">
+
+                                <div className="col-12 mx-auto">
+                                    <div className="card mb-3">
+                                        <div className="row g-0">
+                                            <div className="col-md-12 justify-content-md-center px-0 d-flex align-items-center">
+                                                <div className="card-body">
+                                                    <h5 className="card-title fw-bold pb-1">UPDATE SCHEDULE</h5>
+                                                    <p className="card-text mb-0 pb-2">
+                                                        Updating schedule for the following days: <br /><u>Monday, </u>
+                                                        <u>Tuesday</u>
+                                                    </p>
+                                                    <Form>
+
+                                                        {formValues.map((element, index) => (
+                                                            <div className="row justify-content-center mb-3">
+                                                                <div className="col-4">
+                                                                    <div className="form-floating">
+                                                                        <input name="start_time" type="time" className="form-control ps-3"
+                                                                            placeholder=" " onChange={e => handleChange(index, e)} />
+
+                                                                        <label htmlFor="email">Start Time</label>
+
+                                                                        {/* <ErrorMessage name="email" /> */}
+                                                                    </div>
+                                                                </div>
+                                                                <div className="col-4">
+                                                                    <div className="form-floating">
+                                                                        <input className="form-control ps-3" name="end_time"
+                                                                            placeholder=" " type="time" onChange={e => handleChange(index, e)} />
+
+                                                                        <label htmlFor="password">End Time</label>
+
+                                                                        {/* <ErrorMessage name="password" /> */}
+                                                                    </div>
+                                                                </div>
+
+                                                                {
+                                                                    index ?
+                                                                        <div className="col-2">
+                                                                            <div className="pt-4">
+                                                                                <button
+                                                                                    type="button"
+                                                                                    className="btn btn-link mx-auto"
+                                                                                    style={{ color: "#323335" }}
+                                                                                    onClick={() => removeFormFields()}
+                                                                                ><u>
+                                                                                        <FontAwesomeIcon icon="times-circle" color="maroon" className="hoverable" />
+                                                                                    </u></button>
+
+                                                                            </div>
+                                                                        </div>
+                                                                        : <div className={col}>
+
+                                                                        </div>
+                                                                }
+                                                            </div>
+                                                        ))}
+                                                        <div className="mb-3">
+                                                            <button
+                                                                className="btn btn-link nav-link fw-bold mx-auto"
+                                                                style={{ color: "#323335" }}
+                                                                onClick={() => addFormFields()}
+                                                            ><u>
+                                                                    Add another time slot
+                                                            </u></button>
+                                                        </div>
+
+                                                        <div className="text-center mb-2" >
+                                                            <button type="submit" onClick={handleSubmit}
+                                                                className="btn btn-primary">UPDATE SCHEDULE</button>
+                                                        </div>
+                                                    </Form>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </LoadingOverlay>
+        </Formik>
+    );
+}
+
+export default UpdateTutorSchedule;
